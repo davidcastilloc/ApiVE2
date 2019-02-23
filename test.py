0880d4e538e757d2900641aecb7a63a65a5b3f99
David Jose <vikruzdavid@hotmail.com>
2019-02-22 23:58:50 -0400

    Actualizado archivo de test

--- test.py
+++ test.py
@@ -9,7 +9,8 @@ class TestFunciones(unittest.TestCase):
     def test_buscar_devuelve_diccionario_con_los_datos_del_ciudadano(self):
         ciudadano = {}
         ciudadano = buscar(24980047)
-        self.assertEqual('DAVID', ciudadano['Nombre1'])
+        print("Funcion de busqueda ejecutada!")
+        self.assertEqual('DAVID', ciudadano['primerNombre'])
 
 if __name__ == '__main__':
     unittest.main()
